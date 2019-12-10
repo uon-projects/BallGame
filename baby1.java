29fb11d2ab411b33962f4ed76841168bd5a1192e
Teodor G <grigor.teodor@gmail.com>
2019-12-10 17:08:55 +0000

    Create baby1.java

---
+++ baby1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class baby1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class baby1 extends Actor
+{
+    /**
+     * Act - do whatever the baby1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        
+    }    
+}
