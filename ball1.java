ca734a95ee0b122b5d30a059000661280246f434
Teodor G <grigor.teodor@gmail.com>
2019-12-13 21:47:48 +0000

    Update ball1.java

--- ball1.java
+++ ball1.java
@@ -11,59 +11,54 @@ public class ball1 extends Actor
     //method that is called when the run or act button is pressed
     //method that draw the actor
     public void act() 
-    {
-        int noRandom;
-        
+    {        
+        //getting the class of the holder world - MyWorld        
         World world = getWorld();
         MyWorld myWorld = (MyWorld)world;
-        Counter counter = myWorld.getCounter();
+        //getting the speed of the ball from the myWorld
         int ballSpeed = myWorld.getBallSpeed();
+        //move the ball by ballSpeed value
+        move(ballSpeed);     
+        //getting the counter from the myWorld so we can interact with the public methods
+        Counter counter = myWorld.getCounter();
         
-        move(ballSpeed);
-        
+        //getting the babies that are in the Team A
+        //getting the baby1 class and store it into a variable
         Actor baby_1 = getOneIntersectingObject(baby1.class);
         if (baby_1!=null)
         {
-            do {
-                noRandom = Greenfoot.getRandomNumber(180);
-            } while(noRandom < 30 || noRandom > 150);
-            turn(noRandom + 90);
+            turn(generateTurnDegree() + 90);
             do {
                 move(10);
             } while(getOneIntersectingObject(baby1.class) != null);
         }
         
+        //getting the baby3 class and store it into a variable
         Actor baby_3 = getOneIntersectingObject(baby3.class);
         if (baby_3!=null)
         {
-            do {
-                noRandom = Greenfoot.getRandomNumber(180);
-            } while(noRandom < 30 || noRandom > 150);
-            turn(noRandom + 90);
+            turn(generateTurnDegree() + 90);
             do {
                 move(10);
             } while(getOneIntersectingObject(baby3.class) != null);
         }
         
+        //getting the babies that are in the Team B
+        //getting the baby2 class and store it into a variable
         Actor baby_2 = getOneIntersectingObject(baby2.class);
         if (baby_2!=null)
         {
-            do {
-                noRandom = Greenfoot.getRandomNumber(180);
-            } while(noRandom < 30 || noRandom > 150);
-            turn(noRandom + 90);
+            turn(generateTurnDegree() + 90);
             do {
                 move(10);
             } while(getOneIntersectingObject(baby2.class) != null);
         }
         
+        //getting the baby4 class and store it into a variable
         Actor baby_4 = getOneIntersectingObject(baby4.class);
         if (baby_4!=null)
         {
-            do {
-                noRandom = Greenfoot.getRandomNumber(180);
-            } while(noRandom < 30 || noRandom > 150);
-            turn(noRandom + 90);
+            turn(generateTurnDegree() + 90);
             do {
                 move(10);
             } while(getOneIntersectingObject(baby4.class) != null);
@@ -92,6 +87,14 @@ public class ball1 extends Actor
             baby4.setLocation(450, 300);
         }
         
+    }
+    
+    //method that retrives a number
+    //this number represents the turn degree
+    //the number returned is in a range from 30 to 150
+    public int generateTurnDegree()
+    {
+        return Greenfoot.getRandomNumber(120) + 30;
     }
     
     public void bounceoffwall()
