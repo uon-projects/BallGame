bdc68655016fffdab4b46fddc6514166f4c1c571
Teodor G <grigor.teodor@gmail.com>
2019-12-13 09:55:49 +0000

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -8,18 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Counter extends Actor
 {
-    int score = 0;
+    int scoreTeamA = 0, scoreTeamB = 0;
     /**
      * Act - do whatever the Counter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        setImage(new GreenfootImage("Score: " + score, 24, Color.GREEN, Color.BLACK));
+        setImage(new GreenfootImage(" Team A vs Team B \n" + scoreTeamA + "/" + scoreTeamB, 24, Color.GREEN, Color.BLACK));
     }
     
-    public void addScore()
+    public void addScoreToA()
     {
-        score++;
+        scoreTeamA++;
+    }
+    
+    public void addScoreToB()
+    {
+        scoreTeamB++;
     }
 }
