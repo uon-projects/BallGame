8c0f6c1fc1f545afc3eb094e34a5c86145d57362
Teodor G <grigor.teodor@gmail.com>
2019-12-13 21:38:06 +0000

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -3,26 +3,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Counter here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Teodor Grigor) 
+ * @version (v1.0)
  */
 public class Counter extends Actor
 {
+    //declare the variables that keeps the score for both teams (team A and team B)
+    //we initialise them to 0
     int scoreTeamA = 0, scoreTeamB = 0;
-    /**
-     * Act - do whatever the Counter wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    //method that its called in order to draw the counter
     public void act()
     {
+        //creating the counter
         setImage(new GreenfootImage(" Team A vs Team B \n" + scoreTeamA + "        :        " + scoreTeamB, 24, Color.WHITE, Color.BLACK));
     }
     
+    //public method that can be accessed outside of this classes
+    //this method increase the Team A score by 1
     public void addScoreToA()
     {
         scoreTeamA++;
     }
     
+    //public method that can be accessed outside of this classes
+    //this method increase the Team B score by 1
     public void addScoreToB()
     {
         scoreTeamB++;
