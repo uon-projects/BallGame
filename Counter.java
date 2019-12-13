912f1efcc4a4715612d7a5965daafd2b48764e72
Teodor G <grigor.teodor@gmail.com>
2019-12-13 09:26:50 +0000

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Counter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Counter extends Actor
+{
+    int score = 0;
+    /**
+     * Act - do whatever the Counter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setImage(new GreenfootImage("Score: " + score, 24, Color.GREEN, Color.BLACK));
+    }    
+}
